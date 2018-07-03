b0625dea399cfd9ac2f0867734a1c31f71cd1a05
Lars Müller-Gilberger <larsmg@web.de>
2018-07-03 21:22:13 +0200

    Integrate a proxy layer for reverse proxying like cluster proxy for kubernetes (#6)

--- run.sh
+++ run.sh
@@ -14,7 +14,12 @@ echo "$STREAMUSER:$STREAMPW" > /usr/local/nginx/conf/.htpasswd_stream
 echo "$STATSUSER:$STATSPW" > /usr/local/nginx/conf/.htpasswd_stats
 
 TARGET="${TARGET:-localhost}"
+HTTPTARGET="${HTTPTARGET:-}"
 KEY="${KEY:-key}"
+
+if [ "$HTTPTARGET" == "" ]; then
+  HTTPTARGET=$TARGET
+fi
 
 for p in /srv/www/*.html ; do
   sed -i -e "s/___TARGET___/$TARGET/g" $p
