78bc17cfe56e3e45f56d422794d1152fab9bc31f
Lars Mueller-Gilberger <larsmg@web.de>
2018-07-03 22:07:51 +0200

    changing back to single target

--- run.sh
+++ run.sh
@@ -14,17 +14,11 @@ echo "$STREAMUSER:$STREAMPW" > /usr/local/nginx/conf/.htpasswd_stream
 echo "$STATSUSER:$STATSPW" > /usr/local/nginx/conf/.htpasswd_stats
 
 TARGET="${TARGET:-localhost}"
-HTTPTARGET="${HTTPTARGET:-}"
 KEY="${KEY:-key}"
 
 for p in /srv/www/*.html ; do
   sed -i -e "s/___TARGET___/$TARGET/g" $p
   sed -i -e "s/___KEY___/$KEY/g" $p 
-  if [ "$HTTPTARGET" == "" ]; then
-    sed -i -e "s/___HTTPTARGET___/$TARGET/g" $p
-  else
-    sed -i -e "s/___HTTPTARGET___/$HTTPTARGET/g" $p
-  fi
 done
 
 nginx
