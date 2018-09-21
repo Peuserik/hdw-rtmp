67c2a56e5b65ad3e07f9401189ea840f4fb37340
Lars Mueller-Gilberger <larsmg@web.de>
2018-09-22 00:42:46 +0200

    Changing the button creation to exec-on-publish

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+#
+
+function=$1
+STREAM=$2
+DATE=`date +%Y.%m.%d-%H:%M:%S`
+
+echo "$DATE ;Function: $function " >> /srv/www/log/stream.log
+echo "$DATE ;STREAM: $STREAM " >> /srv/www/log/stream.log
+
+if [ "$function" == "ADD" ]; then
+    cp /srv/www/dynamic2.html /tmp
+    sed -i "/'konf',/a '$STREAM'," /tmp/dynamic2.html
+    cat /tmp/dynamic2.html >/srv/www/dynamic2.html
+elif [ "$function" == "REMOVE" ]; then
+    cp /srv/www/dynamic2.html /tmp
+    sed -i "/'$STREAM',/d" /tmp/dynamic2.html
+    cat /tmp/dynamic2.html >/srv/www/dynamic2.html
+else
+    echo "$DATE ; NO HIT! WRONG PARAMETERS FOR SCRIPT." >> /srv/www/log/stream.log
+fi
