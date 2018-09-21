ff5f5d9b290d23e1e26836442d25f934a7ec255b
Lars Mueller-Gilberger <larsmg@web.de>
2018-09-22 01:42:23 +0200

    Fix logic issues

--- publish.sh
+++ publish.sh
@@ -9,19 +9,19 @@ DATE=`date +%Y.%m.%d-%H:%M:%S`
 
 
 
-echo "$DATE ;Function: $function " >> /srv/www/log/stream.log
-echo "$DATE ;STREAM: $STREAM " >> /srv/www/log/stream.log
+echo "$DATE ;Function: $function " >> /srv/www/streams/logs/stream.log
+echo "$DATE ;STREAM: $STREAM " >> /srv/www/streams/logs/stream.log
 
 if [ "$function" == "ADD" ]; then
-    cp /srv/www/dynamic2.html /tmp
+    cp /srv/www/dynamic.html /tmp
     sed -i "/'konf',/a '$STREAM'," /tmp/dynamic.html
-    cat /tmp/dynamic2.html >/srv/www/dynamic.html
+    cat /tmp/dynamic.html >/srv/www/dynamic.html
 elif [ "$function" == "REMOVE" ]; then
-    cp /srv/www/dynamic2.html /tmp
+    cp /srv/www/dynamic.html /tmp
     sed -i "/'$STREAM',/d" /tmp/dynamic.html
-    cat /tmp/dynamic2.html >/srv/www/dynamic.html
+    cat /tmp/dynamic.html >/srv/www/dynamic.html
 else
-    echo "$DATE ; NO HIT! WRONG PARAMETERS FOR SCRIPT." >> /srv/www/log/stream.log
+    echo "$DATE ; NO HIT! WRONG PARAMETERS FOR SCRIPT." >> /srv/www/streams/logs/stream.log
 fi
 
 
